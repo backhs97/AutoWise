d5a147093fad797abf1c4dcd29208a36e2e7ed93
v0x <116308110+v0x@users.noreply.github.com>
2024-04-18 22:51:11 -0700

    Create FavoritesScreen.jsx

---
+++ Front/app/FavoritesScreen.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { View, Text } from "react-native";
+
+const FavoritesScreen = () => {
+  return (
+    <View>
+      <Text>Favorites Screen</Text>
+    </View>
+  );
+};
+
+export default FavoritesScreen;
