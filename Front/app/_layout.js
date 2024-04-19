3f4f456735b1493ee3466ee57b14baeaf6be53f2
v0x <116308110+v0x@users.noreply.github.com>
2024-04-18 22:47:30 -0700

    Update _layout.js

--- Front/app/_layout.js
+++ Front/app/_layout.js
@@ -1,7 +1,26 @@
-import { Stack } from 'expo-router';
+import React from "react";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+
+import SearchScreen from "./SearchScreen";
+import FavoritesScreen from "./FavoritesScreen";
+import ProfileScreen from "./ProfileScreen";
+
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const Tab = createBottomTabNavigator();
 
 const Layout = () => {
-    return <Stack />;
-}
+  return (
+    <NavigationContainer>
+      <Tab.Navigator initialRouteName="Home">
+        <Tab.Screen name="Home" component={SearchScreen} />
+        <Tab.Screen name="Favorites" component={FavoritesScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default Layout;
+
