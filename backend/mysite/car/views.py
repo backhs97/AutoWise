2c1f4f8e7b874b0d3107bf8bf668fb7ac19288ad
v0x <116308110+v0x@users.noreply.github.com>
2024-04-19 21:10:01 -0700

    Update views.py

--- backend/mysite/car/views.py
+++ backend/mysite/car/views.py
@@ -2,24 +2,21 @@ from django.contrib.auth import get_user_model, login, logout
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers import UserRegisterSerializer, UserLoginSerializer, UserSerializer
+from .serializers import CarSerializer, UserRegisterSerializer, \
+UserLoginSerializer, UserSerializer , FavoritesSerializer, NotificationSerializer
 from rest_framework import permissions, status
 from .validations import custom_validation, validate_email, validate_password
 
-# class ViewUserViewSet(APIView):
-#     def get(self, request, id=None):
-#         if id:
-#             item = models.CustomUser.objects.get(id=id)
-#             serializer = serializers.UserSerializer(item)
-#             return Response({"status": "success", "data": serializer.data},
-#                             status= status.HTTP_200_OK)
-#
-#         return Response({"status": "error", "data": []}, status=status.HTTP_400_BAD_REQUEST)
-#
+from rest_framework import generics
+
+from django.shortcuts import get_object_or_404
+
+
+# for profile screen 
 # class UserProfileViewSet(APIView):
 #     def get(self, request, id=None):
 #         if id:
-#             item = models.CustomUser.objects.get(id=id)
+#             item = models.AppUser.objects.get(id=id)
 #             serializer = serializers.UserSerializer(item)
 #             return Response({"status": "success", "data": serializer.data},
 #                             status= status.HTTP_200_OK)
@@ -56,6 +53,7 @@ class UserLogin(APIView):
 			return Response(serializer.data, status=status.HTTP_200_OK)
 
 
+# for logout button
 class UserLogout(APIView):
 	permission_classes = (permissions.AllowAny,)
 	authentication_classes = ()
@@ -64,6 +62,7 @@ class UserLogout(APIView):
 		return Response(status=status.HTTP_200_OK)
 
 
+# for profile screen
 class UserView(APIView):
 	permission_classes = (permissions.IsAuthenticated,)
 	authentication_classes = (SessionAuthentication,)
@@ -73,61 +72,56 @@ class UserView(APIView):
 		return Response({'user': serializer.data}, status=status.HTTP_200_OK)
 
 
-# class UpdateUserViewSet(APIView):
-#     def patch(self, request, id=None):
-#         item = models.CustomUser.objects.get(id=id)
-#         serializer = serializers.UserSerializer(item, data=request.data, partial=True)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
-#         else:
-#             return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 
-# class DeleteUserViewSet(APIView):
-#     def delete(self, request, id=None):
-#         item = models.CustomUser.objects.filter(id=id)
-#         print(item)
-#         item.delete()
-#         return Response({"status": "success", "data": "Item Deleted"})
+# for delete account button
+class DeleteUserViewSet(APIView):
+    def delete(self, request, id=None):
+        item = models.AppUser.objects.filter(id=id)
+        print(item)
+        item.delete()
+        return Response({"status": "success", "data": "Item Deleted"})
 
 
-# class CarListViewSet(APIView):
-#     def get(self, request, id=None):
-#         if id:
-#             item = models.Car.objects.get(id=id)
-#             serializer = serializers.CarSerializer(item)
-#             return Response({"status": "success", "data": serializer.data},
-#                             status= status.HTTP_200_OK)
-#
-#         items = models.Car.objects.all()
-#         serializer = serializers.CarSerializer(items, many=True)
-#         return Response({"status": "success", "data": serializer.data},
-#                         status=status.HTTP_200_OK)
-#
-# class AddCarViewSet(APIView):
-#     def post(self, request, id=None):
-#         serializer = serializers.CarSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
-#         else:
-#             return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
-#
-# class UpdateCarViewSet(APIView):
-#     def patch(self, request, id=None):
-#         item = models.Car.objects.get(id=id)
-#         serializer = serializers.UserSerializer(item, data=request.data, partial=True)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
-#         else:
-#             return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
-#
-# class DeleteCarViewSet(APIView):
-#     def delete(self, request, id=None):
-#         item = models.Car.objects.filter(id=id)
-#         print(item)
-#         item.delete()
-#         return Response({"status": "success", "data": "Item Deleted"})
-#
+# for favorites screen
+class CarListViewSet(APIView):
+    def get(self, request, id=None):
+        if id:
+            item = models.Car.objects.get(id=id)
+            serializer = serializers.CarSerializer(item)
+            return Response({"status": "success", "data": serializer.data},
+                            status= status.HTTP_200_OK)
+
+        items = models.Car.objects.all()
+        serializer = serializers.CarSerializer(items, many=True)
+        return Response({"status": "success", "data": serializer.data},
+                        status=status.HTTP_200_OK)
+
+# for favoriting car listing
+class SaveCarView(APIView):
+     def post(self, request, car_id):
+
+        car = get_object_or_404(Car, pk=car_id)
+
+        user = request.user
+
+        # Check if the car is already favorited by the user
+        if Favorites.objects.filter(user=user, car=car).exists():
+            return Response({"status": "error", "message": "Car is already favorited by the user"}, status=status.HTTP_400_BAD_REQUEST)
+
+        Favorites.objects.create(user=user, car=car)
+        return Response({"status": "success", "message": "Car favorited successfully"}, status=status.HTTP_201_CREATED)
+
+
+# for removing favorite car listing
+class DeleteCarView(APIView):
+    def delete(self, request, car_id):
+        car = get_object_or_404(Car, pk=car_id)
+
+        car.delete()
+        return Response({"status": "success", "message": "Car deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+
+# web crawler for searching
+class SearchViewSet(APIView):
+    def get(self, request):
+        pass
