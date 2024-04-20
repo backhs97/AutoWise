edddf7f364e60e9d1878168fc5aeffad7a381b0d
v0x <116308110+v0x@users.noreply.github.com>
2024-04-19 20:36:28 -0700

    Update LoginScreen.jsx

--- Front/app/LoginScreen.jsx
+++ Front/app/LoginScreen.jsx
@@ -4,113 +4,101 @@ import React from "react";
 import {
   StyleSheet,
   SafeAreaView,
-  ScrollView,
   View,
   Text,
   TextInput,
   Button,
-  TouchableOpacity,
 } from "react-native";
 
 import { COLORS } from "../constants/theme";
 
-import { Formik } from "formik";
 import * as yup from "yup";
 
 axios.defaults.xsrfCookieName = "csrftoken";
 axios.defaults.xsrfHeaderName = "X-CSRFToken";
 axios.defaults.withCredentials = true;
 
-const signInValidationSchema = yup.object().shape({
-  email: yup
-    .string()
-    .email("Please enter valid email")
-    .required("Email Address is Required"),
-  password: yup
-    .string()
-    .min(8, ({ min }) => `Password must be at least ${min} characters`)
-    .required("Password is required"),
-});
+// const signInValidationSchema = yup.object().shape({
+//   email: yup
+//     .string()
+//     .email("Please enter valid email")
+//     .required("Email Address is Required"),
+//   password: yup
+//     .string()
+//     .min(8, ({ min }) => `Password must be at least ${min} characters`)
+//     .required("Password is required"),
+// });
 
 const LoginScreen = ({ navigation }) => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (name, value) => {
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async () => {
+    const { email, password } = formData;
+    try {
+      const response = await axios.post(
+        "http://127.0.0.1:8000/api/login",
+        formData,
+      );
+      console.log("login successful:", response.data);
+      console.log("Form submitted with data:", formData);
+      navigation.navigate("Search");
+    } catch (error) {
+      console.error("Registration error:", error);
+    }
+  };
+
   return (
-    <SafeAreaView
-      style={{
-        display: "flex",
-        alignItems: "center",
-        flex: 1,
-        backgroundColor: COLORS.secondary,
-        justifyContent: "center",
-      }}
-    >
-      <Text>Register Screen</Text>
-      <Formik
-        validationSchema={signInValidationSchema}
-        initialValues={{
-          email: "",
-          password: "",
-          confirmPassword: "",
-        }}
-        onSubmit={(values) => console.log(values)}
-      >
-        {({
-          errors,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          values,
-          isValid,
-        }) => (
-          <>
-            <TextInput
-              name="email"
-              style={{
-                height: 40,
-                borderWidth: 1,
-                padding: 10,
-                marginTop: 20,
-                borderRadius: 5,
-              }}
-              placeholder="Email Address"
-              onChangeText={handleChange("email")}
-              onBlur={handleBlur("email")}
-              value={values.email}
-              keyboardType="email-address"
-            />
-            {errors.email && (
-              <Text style={{ fontSize: 10, color: "red" }}>{errors.email}</Text>
-            )}
-            <TextInput
-              name="password"
-              style={{
-                height: 40,
-                borderWidth: 1,
-                padding: 10,
-                marginTop: 20,
-                borderRadius: 5,
-              }}
-              placeholder="Password"
-              onChangeText={handleChange("password")}
-              onBlur={handleBlur("password")}
-              value={values.password}
-              secureTextEntry
-            />
-            {errors.password && (
-              <Text style={{ fontSize: 10, color: "red" }}>
-                {errors.password}
-              </Text>
-            )}
-            <Button onPress={handleSubmit} title="LOGIN" disabled={!isValid} />
-            <Text>Register?</Text>
-            <Button
-              onPress={() => navigation.navigate("Register")}
-              title="Register"
-            />
-          </>
-        )}
-      </Formik>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email Address"
+        onChangeText={(text) => handleChange("email", text)}
+        value={formData.email}
+        keyboardType="email-address"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        onChangeText={(text) => handleChange("password", text)}
+        value={formData.password}
+        secureTextEntry
+      />
+      <Button onPress={handleSubmit} title="Login" />
+      <Button
+        onPress={() => navigation.navigate("Register")}
+        title="Register"
+      />
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 20,
+  },
+  input: {
+    height: 40,
+    width: "80%",
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  },
+});
 
 export default LoginScreen;
