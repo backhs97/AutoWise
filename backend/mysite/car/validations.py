4a99d43db32e8efab2af41e817995c719a10d54c
v0x <116308110+v0x@users.noreply.github.com>
2024-04-19 20:39:35 -0700

    Update validations.py

--- backend/mysite/car/validations.py
+++ backend/mysite/car/validations.py
@@ -4,7 +4,6 @@ UserModel = get_user_model()
 
 def custom_validation(data):
     email = data['email'].strip()
-    username = data['username'].strip()
     password = data['password'].strip()
     ##
     if not email or UserModel.objects.filter(email=email).exists():
@@ -13,8 +12,6 @@ def custom_validation(data):
     if not password or len(password) < 8:
         raise ValidationError('choose another password, min 8 characters')
     ##
-    if not username:
-        raise ValidationError('choose another username')
     return data
 
 
@@ -24,11 +21,6 @@ def validate_email(data):
         raise ValidationError('an email is needed')
     return True
 
-def validate_username(data):
-    username = data['username'].strip()
-    if not username:
-        raise ValidationError('choose another username')
-    return True
 
 def validate_password(data):
     password = data['password'].strip()
