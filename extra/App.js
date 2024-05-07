08074002626f194f538cb840840120c00926399e
Jake <124315722+backhs97@users.noreply.github.com>
2024-05-07 15:50:58 -0700

    Add files via upload

---
+++ extra/App.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import WelcomeScreen from "./WelcomeScreen";
+import SearchScreen from "./SearchScreen";
+import FavoritesPage from "./FavoritesPage";
+import ResultsScreen from "./ResultsScreen.js";
+import SignUpScreen from "./SignUpScreen";
+import LoginScreen from "./LoginScreen";
+import NotificationPage from "./NotificationPage";
+
+import Icon from "react-native-vector-icons/FontAwesome";
+
+const Stack = createStackNavigator();
+const WelcomeStack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function WelcomeStackNavigator() {
+  return (
+    <WelcomeStack.Navigator>
+      <WelcomeStack.Screen name="Welcome" component={WelcomeScreen}/>
+      <WelcomeStack.Screen name="SignUp" component={SignUpScreen}/>
+      <WelcomeStack.Screen name="Login" component={LoginScreen}/>
+    </WelcomeStack.Navigator>
+  );
+}
+
+function SearchStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Search" component={SearchScreen} />
+      <Stack.Screen name="Results" component={ResultsScreen} />
+    </Stack.Navigator>
+  );
+}
+
+function Navbar() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={WelcomeStackNavigator} options={{
+        tabBarIcon: ({ color, size }) => (
+          <Icon name="home" size={size} color={color} />
+        )
+      }}
+    />
+      <Tab.Screen name="Search" component={SearchStack} options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="search" size={size} color={color} />
+          )
+        }}
+      />
+      <Tab.Screen name="Favorites" component={FavoritesPage} options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="heart" size={size} color={color} />
+          )
+        }}
+      />
+      <Tab.Screen name="Notifications" component={NotificationPage} options={{
+        tabBarIcon: ({ color, size }) => (
+          <Icon name="bell" color={color} size={size} />
+        )
+      }}/>
+    </Tab.Navigator>
+  );
+}
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Navbar />
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ extra/SearchScreen.jsx
@@ -0,0 +1,117 @@
+import React, { useState } from "react";
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert } from "react-native";
+
+const SearchPage = ({ navigation }) => {
+  const [searchParams, setSearchParams] = useState({
+    model: "",
+    make: "",
+    color: "",
+    zipCode: "",
+    price: "",
+    type: "",
+    year: "",
+  });
+
+  const handleSearch = () => {
+    console.log("Search parameters:", searchParams);
+    navigation.navigate("Results", { params: searchParams });
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.headerText}>Search for your desired car</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Make"
+        value={searchParams.make}
+        onChangeText={(text) => setSearchParams({ ...searchParams, make: text })}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Model"
+        value={searchParams.model}
+        onChangeText={(text) => setSearchParams({ ...searchParams, model: text })}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="New or Used"
+        value={searchParams.type}
+        onChangeText={(text) =>
+          setSearchParams({ ...searchParams, type: text })
+        }
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Year"
+        value={searchParams.year}
+        onChangeText={(text) =>
+          setSearchParams({ ...searchParams, year: text })
+        }
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Color"
+        value={searchParams.color}
+        onChangeText={(text) => setSearchParams({ ...searchParams, color: text })}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Zip Code"
+        value={searchParams.zipCode}
+        onChangeText={(text) => setSearchParams({ ...searchParams, zipCode: text })}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Price"
+        value={searchParams.price}
+        onChangeText={(text) => setSearchParams({ ...searchParams, price: text })}
+      />
+      <TouchableOpacity style={styles.button} onPress={handleSearch}>
+        <Text style={styles.buttonText}>Search</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "#fff",
+    alignItems: 'center',  // Center content horizontally
+    justifyContent: 'center'  // Center content vertically
+  },
+  headerText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+    color: 'gray'
+  },
+  input: {
+    height: 40,
+    width: '80%',  // Width of 80% of the screen width
+    marginVertical: 10,
+    paddingLeft: 10,
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    backgroundColor: '#fff'
+  },
+  button: {
+    width: '80%',
+    height: 40,
+    backgroundColor: '#FF6A00',  // Button color similar to your provided image
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 20,
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  }
+});
+
+export default SearchPage;
+
